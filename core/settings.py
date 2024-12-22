098ec4bc76fc0a5d8d99fb40f3536a483ecb07b3
Thomas AWOUNFOUET <thomas.awounfouet@gmail.com>
2024-12-22 15:38:46 +0100

    update settings allowed_hots

--- core/settings.py
+++ core/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-ta+t=13qnrq(+e0&8b9wdvhah)mi=a#ipa64(27yd4myfb2!t)
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['localhost', '127.0.0.1' ,'pamoja-nfn-com.onrender.com']
 
 
 # Application definition
