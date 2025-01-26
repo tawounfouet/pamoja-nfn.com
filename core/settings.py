f44d52e2c10608fb04059e33a86f8e77ea691555
Thomas AWOUNFOUET <thomas.awounfouet@gmail.com>
2025-01-27 00:22:41 +0100

    debug de l'authentification sur safari

--- core/settings.py
+++ core/settings.py
@@ -96,7 +96,11 @@ ROOT_URLCONF = 'core.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [BASE_DIR / 'templates'],
+        #'DIRS': [BASE_DIR / 'templates'],
+        'DIRS': [
+            os.path.join(BASE_DIR, 'templates'),
+            os.path.join(BASE_DIR, 'templates', 'allauth'),
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
